73df92f99698159864313f7324af94b92437ce48
Anshvachhani998 <96714394+Anshvachhani998@users.noreply.github.com>
2025-05-25 18:04:08 +0530

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-from pyrogram import filters
-
-from bot import userbot
-
-@userbot.on_message(filters.private & filters.text & filters.incoming)
-async def userbot_receive_link(client, message):
-    text = message.text.lower()
-    if text == "!pings":
-       await message.reply("🏓 Userbot is running!")
