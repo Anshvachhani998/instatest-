cbe202ed96e871fc6522ed0d134882971ccc600a
Anshvachhani998 <96714394+Anshvachhani998@users.noreply.github.com>
2025-05-25 15:17:19 +0530

    Update userbot.py

--- plugins/userbot.py
+++ plugins/userbot.py
@@ -1,5 +1,5 @@
-from pyrogram import filters
-from bot import userbot
+from pyrogram import userbot, filters
+
 
 @userbot.on_message(filters.private & filters.incoming)
 async def userbot_test(client, message):
