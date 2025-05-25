595936dcf72211c18ce0fec8104873f5d5ad4ded
Anshvachhani998 <96714394+Anshvachhani998@users.noreply.github.com>
2025-05-25 11:55:21 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,9 +1,9 @@
 import os
 
 SESSION = "my_bot"
-API_ID = int(os.getenv("API_ID", "801239"))
-API_HASH = os.getenv("API_HASH", "171e6f1c5140fbe827b6b08")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "653862OrA3AJn8t-YIKy_N_3Z6BsNc")
+API_ID = int(os.getenv("API_ID", "8012239"))
+API_HASH = os.getenv("API_HASH", "71e6f1bf66ed8dcc5140fbe827b6b08")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "67277194738:AAHapce3wJ-kSG3WDYG5oxg9wYxaenYwZ-8")
 LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", "-1002379643238"))
 DUMP_CHANNEL = int(os.getenv("DUMP_CHANNEL", "-1002379643238"))
 PORT = int(os.getenv("PORT", "8080"))
