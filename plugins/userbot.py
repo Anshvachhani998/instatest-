f2ff1f482b5849e211a4d5f6c2f7918193e2a9d0
Anshvachhani998 <96714394+Anshvachhani998@users.noreply.github.com>
2025-05-25 15:19:32 +0530

    Update userbot.py

--- plugins/userbot.py
+++ plugins/userbot.py
@@ -1,7 +1,7 @@
 from pyrogram.handlers import MessageHandler
 
 async def userbot_ping(client, message):
-    if message.text.lower() == "!ping":
+    if message.text.lower() == "!pings":
         await message.reply("🏓 Userbot is running!")
 
 userbot.add_handler(MessageHandler(userbot_ping, filters.private & filters.text & filters.incoming))
