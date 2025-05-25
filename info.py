c3c2c98dcc70df7c444360373015c1422520190a
Anshvachhani998 <96714394+Anshvachhani998@users.noreply.github.com>
2025-05-25 11:55:31 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,6 +1,6 @@
 import os
 
-SESSION = "my_bot"
+SESSION = "my_bot5"
 API_ID = int(os.getenv("API_ID", "8012239"))
 API_HASH = os.getenv("API_HASH", "71e6f1bf66ed8dcc5140fbe827b6b08")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "67277194738:AAHapce3wJ-kSG3WDYG5oxg9wYxaenYwZ-8")
